c7b7595a5a9d0aeefdfb48112e82ac3ec39be3fa
One-percent-athlete <one.percent.athlete@gmail.com>
2024-04-11 16:38:35 +0900

    add env variables and Procfile

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ dotenv.load_dotenv()
 
 my_email = os.environ.get("my_email")
 password = os.environ.get("password")
-
+flask_key = os.environ.get("flask_key")
+db_uri = os.environ.get("db_uri")
 '''
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
@@ -36,7 +37,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = flask_key
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -63,7 +64,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts_final.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
